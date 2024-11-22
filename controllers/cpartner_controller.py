d549ca3b348896f07d9c808cc440bcb0c2251953
Chris Apps <chrisapps@Chriss-MacBook-Air.local>
2024-11-22 11:17:13 +1000

    POST route created and tested

--- controllers/cpartner_controller.py
+++ controllers/cpartner_controller.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
 from init import db
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
 
 from models.cpartners import Partner, PartnerSchema
 
@@ -30,7 +32,32 @@ def get_partner(partner_id):
         return {"message": f"Partner with id '{partner_id}' does not exist"}, 404
 
 # Create - /partners - POST
-
+@partners_bp.route("/", methods=["POST"])
+def create_partner():
+    # Exception handling for error
+    try:
+        # Get information from request body
+        body_data = request.get_json()
+        # Create partner instance
+        new_partner = Partner(
+            name = body_data.get("name"),
+            club = body_data.get("club"),
+            address = body_data.get("address"),
+            email = body_data.get("email")
+        )
+        # Add
+        db.session.add(new_partner)
+        # Commit
+        db.session.commit()
+        return PartnerSchema().dump(new_partner), 201
+    except IntegrityError as err:
+        print(err.orig.pgcode)
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            # not_null_violation
+            # Return specific field that is in violation
+            return {"message": f"The field '{err.orig.diag.column_name}' is required"}, 409
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {"message": "Data already exists. Update course details instead"}, 409
 
 # Update - /partner/id - PUT or PATCH
 
