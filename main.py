f56346b726535e88d411a938737bc46634e62b5e
Chris Apps <chrisapps@Chriss-MacBook-Air.local>
2024-11-25 14:20:27 +1000

    javascript finalised to allow edited on front-end

--- main.py
+++ main.py
@@ -39,3 +39,8 @@ def create_app():
         return render_template("index.html")
 
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
+    app.run(host="0.0.0.0", port=port)
